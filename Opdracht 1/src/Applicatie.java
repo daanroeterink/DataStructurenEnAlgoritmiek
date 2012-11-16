6e8adc9d314580a5e1d9452689e5a50209478111
Daan Roeterink <daan.roeterink@gmail.com>
2012-11-16 14:25:53 +0100

    added opdracht 2

--- Opdracht 1/src/Applicatie.java
+++ Opdracht 1/src/Applicatie.java
@@ -1,9 +1,48 @@
+import java.util.Random;
+
+import javax.management.BadAttributeValueExpException;
 
 public class Applicatie {
 
-	static void main(String args[]) {
-		
+	int n = 100000;
+	Random random = new Random();
+	int array[] = new int[n];
+
+	public Applicatie() {
+		for (int i = 0; i < n; i++) {
+			int randomNumber = random.nextInt(n);
+			for (int j = 0; j < i; j++) {
+				if (array[j] == randomNumber) {
+					j = -1;
+					randomNumber = random.nextInt(n);
+				}
+			}
+			array[i] = randomNumber;
+		}
+
+//		try {
+//			doubleCheck(array);
+//		} catch (BadAttributeValueExpException e) {
+//			e.printStackTrace();
+//		}
+		for (int i : array) {
+			System.out.print(i + "|");
+		}
 	}
 	
+	public void doubleCheck(int[] checkArray) throws BadAttributeValueExpException {
+		for(int i =0; i < checkArray.length; i++) {
+			int value = checkArray[i];
+			for(int j = 0; j < checkArray.length; j++) {
+				if(j != i)
+				if(checkArray[j] == value)
+					throw new BadAttributeValueExpException(new Object());
+			}
+		}
+	}
 	
+
+	public static void main(String args[]) {
+		new Applicatie();
+	}
 }
