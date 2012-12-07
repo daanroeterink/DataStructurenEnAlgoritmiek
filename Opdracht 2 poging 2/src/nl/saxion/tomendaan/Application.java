2d9e11ed4f65c47e4a97a69548bb5f87c951f066
Daan Roeterink <daan.roeterink@gmail.com>
2012-12-07 14:09:46 +0100

    changed project 2

--- Opdracht 2 poging 2/src/nl/saxion/tomendaan/Application.java
+++ Opdracht 2 poging 2/src/nl/saxion/tomendaan/Application.java
@@ -1,19 +1,28 @@
 package nl.saxion.tomendaan;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Application {
 
 	public static final String FILENAME = "input.txt";
+	public static final String OUTFILENAME = "output.txt";
 	private Scanner scanner ;
 	private TomEnDaanHeapDeadSpace hpd;
+	BufferedWriter writer;
 
 	public Application() {
 		try {
 			scanner = new Scanner(new File(Application.FILENAME));
+			writer =  new BufferedWriter(new FileWriter(OUTFILENAME));
 		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		RandomFileNumberGenerator file = new RandomFileNumberGenerator();
@@ -35,10 +44,37 @@ public class Application {
 
 	public int getNextNumber() {
 		if (scanner.hasNext()) {
-			return Integer.parseInt(scanner.next());
+			String t = scanner.next();
+			System.out.println("in: " + t);
+			return Integer.parseInt(t);
 		}
 		return -1;
 	}
+	
+	public void writeToFile(int toWrite) {
+		try {
+			writer.write(toWrite + " ");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void newRun() {
+		try {
+			writer.newLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void closeFile() {
+		try {
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 
 	public static void main(String[] args) {
 		new Application();
--- Opdracht 2 poging 2/src/nl/saxion/tomendaan/TomEnDaanHeapDeadSpace.java
+++ Opdracht 2 poging 2/src/nl/saxion/tomendaan/TomEnDaanHeapDeadSpace.java
@@ -6,21 +6,29 @@ package nl.saxion.tomendaan;
  */
 public class TomEnDaanHeapDeadSpace {
 
-	public static final int mSize = 6;
+	public static final int mSize = 8;
 	private int heapSize, deadspaceSize;
 	private int[] memory = new int[mSize];
 	private int[] randomNumbers;
 	private Application app;
-	private int runs = 0;
+	private int runs = 1;
+	private int lastPOP;
 
 	public TomEnDaanHeapDeadSpace(int[] randomNumbers, Application app) {
 		this.app = app;
 		this.randomNumbers = randomNumbers;
 		heapSize = mSize;
 		buildHeap();
+		printArray();
+		for(int i = 0; i < 100; i++) {
+			push();
+			//printArray();
+		}
+		app.closeFile();
 	}
 
 	public void buildHeap() {
+		System.out.println("buildheap");
 		for (int i = 0; i < heapSize; i++) {
 			memory[i] = randomNumbers[i];
 			percolateUp(i);
@@ -29,7 +37,7 @@ public class TomEnDaanHeapDeadSpace {
 
 	public void percolateUp(int location) {
 		// percolate up
-		while (memory[location] < memory[(location - 1) / 2]) {
+		while (memory[location] > memory[(location - 1) / 2]) {
 			int temp = memory[location];
 			memory[location] = memory[(location - 1) / 2];
 			memory[(location - 1) / 2] = temp;
@@ -40,15 +48,48 @@ public class TomEnDaanHeapDeadSpace {
 
 	public int popHeap() {
 		int returnValue = memory[0];
-		int nextNumber = app.getNextNumber();
-		if (nextNumber != -1) {
-			memory[0] = nextNumber;
-		}
+		lastPOP = returnValue;
+		
 		return returnValue;
 	}
 
+	public void add() {
+		int nextNumber = app.getNextNumber();
+		if (nextNumber != -1) {
+			if (nextNumber > lastPOP) {
+				heapSize--;
+				deadspaceSize++;
+				if(heapSize > 0)
+				{
+					memory[0] = memory[heapSize];
+					percolateDown();
+				}
+				memory[heapSize] = nextNumber;
+				System.out.println("out deadspace: " + nextNumber);
+			} else {
+				memory[0] = nextNumber;
+				System.out.println("out heap: " + nextNumber);
+			}
+		}
+	}
+
 	public void push() {
-		popHeap();
+		int writeValue = popHeap();
+		System.out.println("Write to run writevalue: " + writeValue);
+		this.app.writeToFile(writeValue);
+		//buildHeap();
+		//System.out.println("Heapsize :" + heapSize);
+		//System.out.println("deadSpaceSize: " + deadspaceSize);
+		if (heapSize == 0) {
+			heapSize = mSize;
+			deadspaceSize = 0;
+			buildHeap();
+			runs++;
+			this.app.newRun();
+			System.out.println("runs: " + runs);
+		}
+		add();
+		percolateDown();
 	}
 
 	public void percolateDown() {
@@ -64,7 +105,7 @@ public class TomEnDaanHeapDeadSpace {
 						int temp = memory[position];
 						memory[position] = memory[(position * 2) + 2];
 						memory[(position * 2) + 2] = temp;
-					} 
+					}
 					position = (position * 2) + 2;
 				} else {
 					// left
@@ -87,5 +128,12 @@ public class TomEnDaanHeapDeadSpace {
 			hasLeftChild = (position * 2) + 1 < mSize;
 			hasRightChild = (position * 2) + 2 < mSize;
 		}
+	}
+
+	public void printArray() {
+		for (int i = 0; i < memory.length; i++) {
+			System.out.print(memory[i] + "|");
+		}
+		System.out.print("\r\n");
 	}
 }
