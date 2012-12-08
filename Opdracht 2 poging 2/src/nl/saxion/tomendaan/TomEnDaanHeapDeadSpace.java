0d1e8ccea3571a7e21220e870c8bf8a5a3f0b6ea
Tom Kostense <tom.kostense@gmail.com>
2012-12-08 15:31:09 +0100

    Haalt nu daadwerkelijk steeds nieuwe gegevens op

--- Opdracht 2 poging 2/src/nl/saxion/tomendaan/TomEnDaanHeapDeadSpace.java
+++ Opdracht 2 poging 2/src/nl/saxion/tomendaan/TomEnDaanHeapDeadSpace.java
@@ -4,40 +4,63 @@ package nl.saxion.tomendaan;
  * @author Daan
  * 
  */
-public class TomEnDaanHeapDeadSpace {
+public class TomEnDaanHeapDeadSpace
+{
 
 	public static final int mSize = 8;
+
 	private int heapSize, deadspaceSize;
+
 	private int[] memory = new int[mSize];
+
 	private int[] randomNumbers;
+
 	private Application app;
+
 	private int runs = 1;
+
 	private int lastPOP;
 
-	public TomEnDaanHeapDeadSpace(int[] randomNumbers, Application app) {
+	public TomEnDaanHeapDeadSpace(int[] randomNumbers, Application app)
+	{
 		this.app = app;
 		this.randomNumbers = randomNumbers;
 		heapSize = mSize;
+		initFillMemory();
 		buildHeap();
 		printArray();
-		for(int i = 0; i < 100; i++) {
+		for (int i = 0; i < 300; i++)
+		{
 			push();
-			//printArray();
+			// printArray();
 		}
 		app.closeFile();
 	}
 
-	public void buildHeap() {
-		System.out.println("buildheap");
-		for (int i = 0; i < heapSize; i++) {
+	public void initFillMemory()
+	{
+		System.out.println("initFill");
+		for (int i = 0; i < heapSize; i++)
+		{
 			memory[i] = randomNumbers[i];
 			percolateUp(i);
 		}
 	}
 
-	public void percolateUp(int location) {
+	public void buildHeap()
+	{
+		System.out.println("buildheap");
+		for (int i = 0; i < heapSize; i++)
+		{
+			percolateUp(i);
+		}
+	}
+
+	public void percolateUp(int location)
+	{
 		// percolate up
-		while (memory[location] > memory[(location - 1) / 2]) {
+		while (memory[location] > memory[(location - 1) / 2])
+		{
 			int temp = memory[location];
 			memory[location] = memory[(location - 1) / 2];
 			memory[(location - 1) / 2] = temp;
@@ -46,92 +69,117 @@ public class TomEnDaanHeapDeadSpace {
 		}
 	}
 
-	public int popHeap() {
+	public int popHeap()
+	{
 		int returnValue = memory[0];
 		lastPOP = returnValue;
-		
+
 		return returnValue;
 	}
 
-	public void add() {
+	public void add()
+	{
 		int nextNumber = app.getNextNumber();
-		if (nextNumber != -1) {
-			if (nextNumber > lastPOP) {
+		if (nextNumber != -1)
+		{
+			if (nextNumber > lastPOP)
+			{
 				heapSize--;
 				deadspaceSize++;
-				if(heapSize > 0)
-				{
-					memory[0] = memory[heapSize];
-					percolateDown();
-				}
+				memory[0] = memory[heapSize];
 				memory[heapSize] = nextNumber;
 				System.out.println("out deadspace: " + nextNumber);
-			} else {
+			}
+			else
+			{
 				memory[0] = nextNumber;
+				percolateDown();
 				System.out.println("out heap: " + nextNumber);
 			}
 		}
 	}
 
-	public void push() {
-		int writeValue = popHeap();
-		System.out.println("Write to run writevalue: " + writeValue);
-		this.app.writeToFile(writeValue);
-		//buildHeap();
-		//System.out.println("Heapsize :" + heapSize);
-		//System.out.println("deadSpaceSize: " + deadspaceSize);
-		if (heapSize == 0) {
+	public void push()
+	{
+		if (heapSize > -1)
+		{
+			int writeValue = popHeap();
+			System.out.println("Write to run writevalue: " + writeValue);
+			this.app.writeToFile(writeValue);
+		}
+		// buildHeap();
+		// System.out.println("Heapsize :" + heapSize);
+		// System.out.println("deadSpaceSize: " + deadspaceSize);
+		if (heapSize == 0)
+		{
 			heapSize = mSize;
 			deadspaceSize = 0;
 			buildHeap();
 			runs++;
 			this.app.newRun();
 			System.out.println("runs: " + runs);
+			printArray();
 		}
-		add();
-		percolateDown();
+		else
+		{
+			add();
+			percolateDown();
+		}
 	}
 
-	public void percolateDown() {
+	public void percolateDown()
+	{
 		int position = 0;
-		boolean hasLeftChild = (position * 2) + 1 < mSize;
-		boolean hasRightChild = (position * 2) + 2 < mSize;
+		boolean hasLeftChild = (position * 2) + 1 < heapSize;
+		boolean hasRightChild = (position * 2) + 2 < heapSize;
 
-		while (hasLeftChild || hasRightChild) {
-			if (hasLeftChild && hasRightChild) {
-				if (memory[(position * 2) + 1] < memory[position * 2 + 2]) {
+		while (hasLeftChild || hasRightChild)
+		{
+			if (hasLeftChild && hasRightChild)
+			{
+				if (memory[(position * 2) + 1] < memory[position * 2 + 2])
+				{
 					// right
-					if (memory[position] < memory[(position * 2) + 2]) {
+					if (memory[position] < memory[(position * 2) + 2])
+					{
 						int temp = memory[position];
 						memory[position] = memory[(position * 2) + 2];
 						memory[(position * 2) + 2] = temp;
 					}
 					position = (position * 2) + 2;
-				} else {
+				}
+				else
+				{
 					// left
-					if (memory[position] < memory[(position * 2) + 1]) {
+					if (memory[position] < memory[(position * 2) + 1])
+					{
 						int temp = memory[position];
 						memory[position] = memory[(position * 2) + 1];
 						memory[(position * 2) + 1] = temp;
 					}
 					position = (position * 2) + 1;
 				}
-			} else {
+			}
+			else
+			{
 				// left
-				if (memory[position] < memory[(position * 2) + 1]) {
+				if (memory[position] < memory[(position * 2) + 1])
+				{
 					int temp = memory[position];
 					memory[position] = memory[(position * 2) + 1];
 					memory[(position * 2) + 1] = temp;
 				}
 				position = (position * 2) + 1;
 			}
-			hasLeftChild = (position * 2) + 1 < mSize;
-			hasRightChild = (position * 2) + 2 < mSize;
+			hasLeftChild = (position * 2) + 1 < heapSize;
+			hasRightChild = (position * 2) + 2 < heapSize;
 		}
 	}
 
-	public void printArray() {
-		for (int i = 0; i < memory.length; i++) {
+	public void printArray()
+	{
+		for (int i = 0; i < memory.length; i++)
+		{
 			System.out.print(memory[i] + "|");
 		}
 		System.out.print("\r\n");
