4b89ce3c77d71124a2eb13c5722dbd69d8818591
Tom Kostense <tom.kostense@gmail.com>
2012-12-21 13:25:51 +0100

    Readfile

--- Trie_Opdracht 3/src/nl/saxion/tjksoftware/Application.java
+++ Trie_Opdracht 3/src/nl/saxion/tjksoftware/Application.java
@@ -1,13 +1,28 @@
 package nl.saxion.tjksoftware;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Application
 {
+	public static final String FILENAME = "input.txt";
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args)
+	private Scanner scanner;
+
+	public Application()
 	{
+
+		try
+		{
+			scanner = new Scanner(new File(Application.FILENAME));
+		}
+		catch (FileNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		Trie trie = new Trie();
 		trie.insert("Aap");
 		trie.insert("Noot");
@@ -15,6 +30,14 @@ public class Application
 		trie.insert("Aardappel");
 		trie.insert("Peer");
 		trie.insert("Hond");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		new Application();
 	}
 
 }
