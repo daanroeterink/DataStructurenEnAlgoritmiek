04c9fd112a5a8f0ad30eb28ce9c2e03083a91226
Daan Roeterink <daan.roeterink@gmail.com>
2013-01-10 12:54:15 +0100

    initial setup

---
+++ Opdracht4_PERT/src/Applicatie.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+import nl.saxion.act.graphs.*;
+
+
+public class Applicatie {
+
+	List<Integer> verticles = new ArrayList<Integer>();
+	
+	public Applicatie() {
+		
+	}
+	
+	public static void main(String[] args) {
+		new Applicatie();
+	}
+}
---
+++ Opdracht4_PERT/src/PERTNetwerk.java
@@ -0,0 +1,7 @@
+
+public class PERTNetwerk<V,E> extends nl.saxion.act.graphs.WGT_DiGraph<V, E> {
+	
+	private V data;
+	
+
+}
